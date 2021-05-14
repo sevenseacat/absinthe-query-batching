e7a8949d859a5ccefd4cbbbe8561f471d69ba398
Rebecca Le <traybaby@gmail.com>
2021-05-14 16:28:38 +0800

    Load enrolment counts in a naive way

--- lib/course_loader/courses.ex
+++ lib/course_loader/courses.ex
@@ -1,5 +1,16 @@
 defmodule CourseLoader.Courses do
+  import Ecto.Query
+
   def list_courses do
     CourseLoader.Repo.all(CourseLoader.Courses.Course)
+  end
+
+  def count_course_enrolments(%{id: id}, enrolment_type) do
+    from(
+      e in CourseLoader.Courses.Enrolment,
+      select: count(e.course_id),
+      where: e.course_id == ^id and e.type == ^enrolment_type
+    )
+    |> CourseLoader.Repo.one()
   end
 end
--- lib/course_loader_web/graphql/resolvers.ex
+++ lib/course_loader_web/graphql/resolvers.ex
@@ -1,8 +1,15 @@
 defmodule CourseLoaderWeb.GraphQL.Resolvers do
+  alias CourseLoader.Courses
+
   def list_courses(_, _, _) do
-    {:ok, CourseLoader.Courses.list_courses()}
+    {:ok, Courses.list_courses()}
   end
 
-  def course_auto_enrolment_count(_, _, _), do: {:ok, 0}
-  def course_manual_enrolment_count(_, _, _), do: {:ok, 0}
+  def course_auto_enrolment_count(course, _, _) do
+    {:ok, Courses.count_course_enrolments(course, "auto")}
+  end
+
+  def course_manual_enrolment_count(course, _, _) do
+    {:ok, Courses.count_course_enrolments(course, "manual")}
+  end
 end
