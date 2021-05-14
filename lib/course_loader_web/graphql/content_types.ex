daf9e15a8a962ceaa605f4b698e05d12063c1e01
Rebecca Le <traybaby@gmail.com>
2021-05-14 16:09:48 +0800

    Add custom resolvers for count fields that default to 0

--- lib/course_loader_web/graphql/content_types.ex
+++ lib/course_loader_web/graphql/content_types.ex
@@ -1,9 +1,19 @@
 defmodule CourseLoaderWeb.GraphQL.ContentTypes do
   use Absinthe.Schema.Notation
 
+  alias CourseLoaderWeb.GraphQL.Resolvers
+
   object :course do
     field :id, non_null(:id)
     field :code, non_null(:string)
     field :name, non_null(:string)
+
+    field :auto_enrolment_count, non_null(:integer) do
+      resolve(&Resolvers.course_auto_enrolment_count/3)
+    end
+
+    field :manual_enrolment_count, non_null(:integer) do
+      resolve(&Resolvers.course_manual_enrolment_count/3)
+    end
   end
 end
--- lib/course_loader_web/graphql/resolvers.ex
+++ lib/course_loader_web/graphql/resolvers.ex
@@ -2,4 +2,7 @@ defmodule CourseLoaderWeb.GraphQL.Resolvers do
   def list_courses(_, _, _) do
     {:ok, CourseLoader.Courses.list_courses()}
   end
+
+  def course_auto_enrolment_count(_, _, _), do: {:ok, 0}
+  def course_manual_enrolment_count(_, _, _), do: {:ok, 0}
 end
